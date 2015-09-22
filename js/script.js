a02156b39272f3223e9b3b37080b3c8bab213ded
Tim Sarvey <timsarvey@gmail.com>
2015-09-22 14:55:22 -0400

    Put question objects into an array

--- js/script.js
+++ js/script.js
@@ -1,80 +1,98 @@
-var question1 = {
+var question = [];
+
+question.push({
 	picture: "images/mr-stay-puft.jpg", 
 	answerA: "Godzilla",
 	answerB: "Nightmare on Elm Street",
 	answerC: "Ghostbusters",
 	answerD: "Die Hard",
 	correct: "c"
-}
+});
 
-var question2 = Object.create(question1);
-question2.picture = "images/architect.jpg";
-question2.answerA = "The Matrix Reloaded";
-question2.answerB = "Beetlejuice";
-question2.answerC = "Crocodile Dundee";
-question2.answerD = "Star Trek III: The Search for Spock";
-question2.correct = "a";
+question.push({
+	picture: "images/architect.jpg",
+	answerA: "The Matrix Reloaded",
+	answerB: "Beetlejuice",
+	answerC: "Crocodile Dundee",
+	answerD: "Star Trek III: The Search for Spock",
+	correct: "a"
+});
 
-var question3 = Object.create(question1);
-question3.picture = "images/clubber.jpg";
-question3.answerA = "Southpaw";
-question3.answerB = "Ali";
-question3.answerC = "Raging Bull";
-question3.answerD = "Rocky III";
-question3.correct = "d";
+question.push({
+	picture: "images/clubber.jpg",
+	answerA: "Southpaw",
+	answerB: "Ali",
+	answerC: "Raging Bull",
+	answerD: "Rocky III",
+	correct: "d"
+});
 
-var question4 = Object.create(question1);
-question4.picture = "images/davy-jones.jpg";
-question4.answerA = "Percy Jackson: Sea of Monsters";
-question4.answerB = "Clash of the Titans";
-question4.answerC = "Creature from the Black Lagoon";
-question4.answerD = "Pirates of the Caribbean";
-question4.correct = "d";
+question.push({
+	picture: "images/davy-jones.jpg",
+	answerA: "Percy Jackson: Sea of Monsters",
+	answerB: "Clash of the Titans",
+	answerC: "Creature from the Black Lagoon",
+	answerD: "Pirates of the Caribbean",
+	correct: "d"
+});
 
-var question5 = Object.create(question1);
-question5.picture = "images/hans.jpg";
-question5.answerA = "Harry Potter and the Order of the Phoenix";
-question5.answerB = "Die Hard";
-question5.answerC = "Robin Hood: Prince of Thieves";
-question5.answerD = "Sweeney Todd";
-question5.correct = "b";
+question.push({
+	picture: "images/hans.jpg",
+	answerA: "Harry Potter and the Order of the Phoenix",
+	answerB: "Die Hard",
+	answerC: "Robin Hood: Prince of Thieves",
+	answerD: "Sweeney Todd",
+	correct: "b"
+});
 
-var question6 = Object.create(question1);
-question6.picture = "images/indy.jpg";
-question6.answerA = "Indiana Jones and the Kingdom of the Crystal Skull";
-question6.answerB = "Indiana Jones and the Last Crusade";
-question6.answerC = "Indiana Jones and the Temple of Doom";
-question6.answerD = "Raiders of the Lost Ark";
-question6.correct = "d";
 
-var question7 = Object.create(question1);
-question7.picture = "images/sauron.jpg";
-question7.answerA = "Lord of the Rings";
-question7.answerB = "Thor";
-question7.answerC = "Guadians of the Galaxy";
-question7.answerD = "The Three Muskateers";
-question7.correct = "a";
+question.push({
+	picture: "images/indy.jpg",
+	answerA: "Indiana Jones and the Kingdom of the Crystal Skull",
+	answerB: "Indiana Jones and the Last Crusade",
+	answerC: "Indiana Jones and the Temple of Doom",
+	answerD: "Raiders of the Lost Ark",
+	correct: "d"
+});
 
-var question8 = Object.create(question1);
-question8.picture = "images/sensai.jpg";
-question8.answerA = "Bloodsport";
-question8.answerB = "Fists of Fury";
-question8.answerC = "Karate Kid";
-question8.answerD = "Kill BIll";
-question8.correct = "c";
 
-var question9 = Object.create(question1);
-question9.picture = "images/shredder.jpg";
-question9.answerA = "The Avengers";
-question9.answerB = "Teenage Mutant Ninja Turtles";
-question9.answerC = "Gladiator";
-question9.answerD = "Robocop";
-question9.correct = "b";
+question.push({
+	picture: "images/sauron.jpg",
+	answerA: "Lord of the Rings",
+	answerB: "Thor",
+	answerC: "Guadians of the Galaxy",
+	answerD: "The Three Muskateers",
+	correct: "a"
+});
 
-var question10 = Object.create(question1);
-question10.picture = "images/t1000.jpg";
-question10.answerA = "Terminator 2: Judgement Day";
-question10.answerB = "I, Robot";
-question10.answerC = "Short Circuit";
-question10.answerD = "Robocop";
-question10.correct = "a";
+question.push({
+	picture: "images/sensai.jpg",
+	answerA: "Bloodsport",
+	answerB: "Fists of Fury",
+	answerC: "Karate Kid",
+	answerD: "Kill BIll",
+	correct: "c"
+});
+
+question.push({
+	picture: "images/shredder.jpg",
+	answerA: "The Avengers",
+	answerB: "Teenage Mutant Ninja Turtles",
+	answerC: "Gladiator",
+	answerD: "Robocop",
+	correct: "b"
+});
+
+question.push({
+	picture: "images/t1000.jpg",
+	answerA: "Terminator 2: Judgement Day",
+	answerB: "I, Robot",
+	answerC: "Short Circuit",
+	answerD: "Robocop",
+	correct: "a"
+});
+
+
+
+
+
